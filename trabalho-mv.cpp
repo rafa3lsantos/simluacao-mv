b7f830d68a14a931c954420b7825117d5f599f87
rafa3lsantos <rafaelsantosdoamaral07@gmail.com>
2025-11-17 22:23:13 -0300

    Estruturas e variaveis implementadas

--- trabalho-mv.cpp
+++ trabalho-mv.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#define TAMANHO_MEMORIA_FISICA 1024
+#define TAMANHO_PAGINA 64
+#define TAMANHO_TLB 4
+#define NUMERO_DE_PAGINAS 32        
+#define NUMERO_DE_SEGMENTOS 5
 
+typedef struct {
+    int frame_fisico;
+    int bit_valid;
+    int bit_dirty;
+} PaginacaoTabelaEntrada;
 
+typedef struct {
+    int base;
+    int limite;
+    int valida;
+} SegmentacaoTabelaEntrada;
+
+typedef struct {
+    int pagina_virtual;
+    int frame_fisico;
+    int valid;
+    int tempo_uso;
+} TlbEntrada;
+
+int memoria_fisica[TAMANHO_MEMORIA_FISICA];
+PaginacaoTabelaEntrada tabela_paginas[NUMERO_DE_PAGINAS];   // A Tabela de Páginas
+SegmentacaoTabelaEntrada tabela_segmentos[NUMERO_DE_SEGMENTOS]; // A Tabela de Segmentos
+TlbEntrada tlb[TAMANHO_TLB];
+
+void inicializar() {
+    
+}
 
 int main() {
-    int memoria_fisica[] = {24, 23};
 
-    int ano;
+    tabela_paginas[0].frame_fisico = 5;
+    tabela_paginas[0].bit_valid = 1;
+
     return 0;
 }
